8f263d4e67372cf8077f79644cb210f467d62295
Vamshikrishna2525 <yadagirivamshikrishna2525@gmail.com>
2026-07-09 18:56:01 +0530

    Allow Vercel frontend

--- inventory-backend/src/main/java/com/wexa/inventory/config/SecurityConfig.java
+++ inventory-backend/src/main/java/com/wexa/inventory/config/SecurityConfig.java
@@ -62,8 +62,13 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(
-                List.of("http://localhost:5173"));
+//        configuration.setAllowedOrigins(
+//                List.of("http://localhost:5173"));
+        configuration.setAllowedOriginPatterns(
+                List.of(
+                    "http://localhost:5173",
+                    "https://stockflow-mvp-liart.vercel.app"
+                ));
 
         configuration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
